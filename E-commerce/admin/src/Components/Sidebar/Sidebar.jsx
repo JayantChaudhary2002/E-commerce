193d105a56416f0b4cd78aa39a3391cd6e644b19
Jayant Chaudhary <jayantchaudhary2002@gmail.com>
2024-03-20 19:42:12 +0530

    initial commit

---
+++ E-commerce/admin/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'></div>
+  )
+}
+
+export default Sidebar
